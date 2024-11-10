55a58dc1761bd276b2693215f92ce6101bcc3c3c
Abhinav <abhinavmohanan17@gmail.com>
2024-11-10 11:56:03 +0530

    Updates Programs

--- Cycle 3/first_follow.c
+++ Cycle 3/first_follow.c
@@ -1,168 +1,82 @@
 #include <stdio.h>
+
+#include <math.h>
+
 #include <string.h>
 
-int n;
-char prods[50][50];
-char firsts[26][50];
-int is_first_done[26];
-char follows[26][50];
-int is_follow_done[26];
+#include <ctype.h>
 
-int isTerminal(char c)
-{
-    if (c < 65 || c > 90)
-        return 1;
-    return 0;
-}
+#include <stdlib.h>
 
-void first(char nonterm)
-{
-    int index = 0;
-    char curr_firsts[50];
-    for (int i = 0; i < n; i++)
-    {
-        if (prods[i][0] == nonterm)
-        {
-            int curr_prod_index = 2;
-            int flag = 0;
-            while (prods[i][curr_prod_index] != '\0' && flag == 0)
-            {
-                flag = 1;
-                if (isTerminal(prods[i][curr_prod_index]))
-                {
-                    curr_firsts[index] = prods[i][2];
-                    index++;
-                    break;
-                }
-                if (!is_first_done[prods[i][curr_prod_index] - 65])
-                    first(prods[i][curr_prod_index]);
-                int in = 0;
-                while (firsts[prods[i][curr_prod_index] - 65][in] != '\0')
-                {
-                    curr_firsts[index] = firsts[prods[i][curr_prod_index] - 65][in];
-                    if (firsts[prods[i][curr_prod_index] - 65][in] == 'e')
-                    {
-                        curr_prod_index++;
-                        flag = 0;
-                    }
-                    index++;
-                    in++;
-                }
-            }
-        }
-    }
-    curr_firsts[index] = '\0';
-    index++;
-    strcpy(firsts[nonterm - 65], curr_firsts);
-    is_first_done[nonterm - 65] = 1;
-}
-
-void follow(char nonterm)
-{
-    int index = 0;
-    char curr_follows[50];
-    if (nonterm == prods[0][0])
-    {
-        curr_follows[index] = '$';
-        index++;
-    }
-    for (int j = 0; j < n; j++)
-    {
-        int k = 2;
-        int include_lhs_flag;
-        while (prods[j][k] != '\0')
-        {
-            include_lhs_flag = 0;
-            if (prods[j][k] == nonterm)
-            {
-                if (prods[j][k + 1] != '\0')
-                {
-                   if (isTerminal(prods[j][k + 1]))
-                   {
-                    curr_follows[index] = prods[j][k + 1];
-                        index++;
-                        break;
-                    }
-                    int in = 0;
-                    while (firsts[prods[j][k + 1] - 65][in] != '\0')
-                    {
-                        if (firsts[prods[j][k + 1] - 65][in] == 'e')
-                        {
-                            include_lhs_flag = 1;
-                            in++;
-                            continue;
-                        }
-                        int temp_flag = 0;
-                        for (int z = 0; z < index; z++)
-                            if (firsts[prods[j][k + 1] - 65][in] == curr_follows[z])
-                            {
-                                temp_flag = 1;
-                                in++;
-                                break;
-                            }
-                        if (temp_flag)
-                            continue;
-                        curr_follows[index] = firsts[prods[j][k + 1] - 65][in];
-                        index++;
-                        in++;
-                    }
-                }
-                if (prods[j][k + 1] == '\0' || include_lhs_flag == 1)
-                {
-                    if (prods[j][0] != nonterm)
-                    {
-                        if (!is_follow_done[prods[j][0] - 65])
-                            follow(prods[j][0]);
-                        int x = 0;
-                        while (follows[prods[j][0] - 65][x] != '\0')
-                        {
-                            int temp_flag = 0;
-                            for (int z = 0; z < index; z++)
-                                if (follows[prods[j][0] - 65][x] == curr_follows[z])
-                                {
-                                    temp_flag = 1;
-                                    x++;
-                                    break;
-                                }
-                            if (temp_flag)
-                                continue;
-                            curr_follows[index] = follows[prods[j][0] - 65][x];
-                            index++;
-                            x++;
-                        }
-                    }
-                }
-            }
-            k++;
-        }
-    }
-    curr_follows[index] = '\0';
-    index++;
-    strcpy(follows[nonterm - 65], curr_follows);
-    is_follow_done[nonterm - 65] = 1;
-}
-
+int n, m = 0, p, i = 0, j = 0;
+char a[10][10], f[10];
+void follow(char c);
+void first(char c);
 int main()
 {
-    printf("Enter the number of productions\n");
+    int i, z;
+    char c, ch;
+    printf("Enter the no of productions : \n");
     scanf("%d", &n);
-    printf("Enter productions: \n");
-    for (int i = 0; i < n; i++)
-        scanf("%s", prods[i]);
-    for (int i = 0; i < 26; i++)
-        is_first_done[i] = 0;
-    for (int i = 0; i < n; i++)
-        if (is_first_done[prods[i][0] - 65] == 0)
-            first(prods[i][0]);
-    for (int i = 0; i < n; i++)
-        if (is_follow_done[prods[i][0] - 65] == 0)
-            follow(prods[i][0]);
-    printf("Firsts:\n");
-    for (int i = 0; i < 26; i++)
-        if (is_first_done[i])
-            printf("%c : %s\n", i + 65, firsts[i]);
-    printf("Follows:\n");
-    for (int i = 0; i < 26; i++)
-        if (is_follow_done[i])
-            printf("%c : %s\n", i + 65, follows[i]);
+    printf("Enter the productions:\n");
+    for (i = 0; i < n; i++)
+        scanf("%s%c", a[i], &ch);
+    do
+    {
+        m = 0;
+        printf("Enter a variable whose fisrt & follow is to be found:");
+
+        scanf("%c", &c);
+        first(c);
+        printf("First(%c)={", c);
+        for (i = 0; i < m; i++)
+            printf("%c", f[i]);
+        printf("}\n");
+        strcpy(f, " ");
+        m = 0;
+        follow(c);
+        printf("Follow(%c)={", c);
+        for (i = 0; i < m; i++)
+            printf("%c", f[i]);
+        printf("}\n");
+        printf("Want to continue or not(1/0) ? ");
+        scanf("%d%c", &z, &ch);
+    } while (z == 1);
+    return (0);
+}
+void first(char c)
+{
+    int k;
+    if (!isupper(c))
+        f[m++] = c;
+    for (k = 0; k < n; k++)
+    {
+        if (a[k][0] == c)
+        {
+            if (a[k][2] == '$')
+                follow(a[k][0]);
+            else if (islower(a[k][2]))
+                f[m++] = a[k][2];
+            else
+                first(a[k][2]);
+        }
+    }
+}
+void follow(char c)
+{
+    if (a[0][0] == c)
+        f[m++] = '$';
+    for (i = 0; i < n; i++)
+    {
+        for (j = 2; j < strlen(a[i]); j++)
+        {
+            if (a[i][j] == c)
+            {
+                if (a[i][j + 1] != '\0')
+                    first(a[i][j + 1]);
+                if (a[i][j + 1] == '\0' && c != a[i][0])
+                    follow(a[i][0]);
+            }
+        }
+    }
 }
--- Cycle 3/recursive_descent.c
+++ Cycle 3/recursive_descent.c
@@ -1,192 +1,70 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<ctype.h>
+#include <stdio.h>
 
-char ip_sym[15],ip_ptr=0,op[50],tmp[50];
-void e_prime();
-void e();
-void t_prime();
-void t();
-void f();
-void advance();
-int n=0;
-void e()
-{
- strcpy(op,"TE'");
- printf("E=%-25s",op);
- printf("E->TE'\n");
- t();
- e_prime();
-}
-void e_prime()
-{
-int i,n=0,l;
-for(i=0;i<=strlen(op);i++)
-    if(op[i]!='e')
- tmp[n++]=op[i];
-strcpy(op,tmp);
-l=strlen(op);
-for(n=0;n < l && op[n]!='E';n++);
-if(ip_sym[ip_ptr]=='+')
- {
-     i=n+2;
- do
- {
- op[i+2]=op[i];
- i++;
- }while(i<=l);
-  op[n++]='+';
-  op[n++]='T';
-  op[n++]='E';
-  op[n++]=39;
-  printf("E=%-25s",op);
-  printf("E'->+TE'\n");
-  advance();
-  t();
-  e_prime();
- }
- else
- {
-     op[n]='e';
-  for(i=n+1;i<=strlen(op);i++)
- op[i]=op[i+1];
- printf("E=%-25s",op);
- printf("E'->e");
- }
-}
-void t()
-{
- int i,n=0,l;
- for(i=0;i<=strlen(op);i++)
-  if(op[i]!='e')
-   tmp[n++]=op[i];
- strcpy(op,tmp);
- l=strlen(op);
-for(n=0;n < l && op[n]!='T';n++);
- i=n+1;
- do
- {
-  op[i+2]=op[i];
-  i++;
- }while(i < l);
- op[n++]='F';
- op[n++]='T';
- op[n++]=39;
- printf("E=%-25s",op);
- printf("T->FT'\n");
- f();
- t_prime();
-}
+#include <string.h>
 
-void t_prime()
-{
-int i,n=0,l;
-for(i=0;i<=strlen(op);i++)
-    if(op[i]!='e')
- tmp[n++]=op[i];
-strcpy(op,tmp);
-l=strlen(op);
-for(n=0;n < l && op[n]!='T';n++);
-if(ip_sym[ip_ptr]=='*')
- {
-     i=n+2;
- do
- {
- op[i+2]=op[i];
- i++;
- }while(i < l);
-  op[n++]='*';
-  op[n++]='F';
-  op[n++]='T';
-  op[n++]=39;
-  printf("E=%-25s",op);
-  printf("T'->*FT'\n");
-  advance();
-  f();
-  t_prime();
- }
- else
- {
-   op[n]='e';
-  for(i=n+1;i<=strlen(op);i++)
- op[i]=op[i+1];
- printf("E=%-25s",op);
- printf("T'->e\n");
- }
-}
+#include <ctype.h>
 
-void f()
-{
-int i,n=0,l;
-for(i=0;i<=strlen(op);i++)
-    if(op[i]!='e')
- tmp[n++]=op[i];
- strcpy(op,tmp);
-l=strlen(op);
-for(n=0;n < l && op[n]!='F';n++);
- if((ip_sym[ip_ptr]=='i')||(ip_sym[ip_ptr]=='I'))
- {
- op[n]='i';
- printf("E=%-25s",op);
- printf("F->i\n");
- advance();
- }
- else
- {
-  if(ip_sym[ip_ptr]=='(')
-  {
-   advance();
-   e();
-   if(ip_sym[ip_ptr]==')')
-   {
-    advance();
-     i=n+2;
- do
- {
- op[i+2]=op[i];
- i++;
- }while(i<=l);
-  op[n++]='(';
-  op[n++]='E';
-  op[n++]=')';
-  printf("E=%-25s",op);
-  printf("F->(E)\n");
-   }
-  }
-  else
-  {
-   printf("\n\t syntax error");
-     exit(1);
-  }
- }
-}
-
-void advance()
-{
- ip_ptr++;
-}
+char input[10];
+int i, error;
+void E();
+void T();
+void Eprime();
+void Tprime();
+void F();
 void main()
 {
- int i;
-  printf("\nGrammar without left recursion");
- printf("\n\t\t E->TE' \n\t\t E'->+TE'|e \n\t\t T->FT' ");
- printf("\n\t\t T'->*FT'|e \n\t\t F->(E)|i");
- printf("\n Enter the input expression:");
- scanf("%s",ip_sym);
- printf("Expressions");
- printf("\t Sequence of production rules\n");
-  e();
-  for(i=0;i < strlen(ip_sym);i++)
- {  if(ip_sym[i]!='+'&&ip_sym[i]!='*'&&ip_sym[i]!='('&& ip_sym[i]!=')'&&ip_sym[i]!='i'&&ip_sym[i]!='I')
-  {
-   printf("\nSyntax error");
-   break;
-  }
-  for(i=0;i<=strlen(op);i++)
-    if(op[i]!='e')
- tmp[n++]=op[i];
-    strcpy(op,tmp);
-    printf("\nE=%-25s",op);
- }
+    i = 0;
+    error = 0;
+    printf("Enter an arithmetic expression : ");
+    gets(input);
+    E();
+    if (strlen(input) == i && error == 0)
+        printf("\nAccepted..!!!\n");
+    else
+        printf("\nRejected..!!!\n");
+}
+
+void E()
+{
+    T();
+    Eprime();
+}
+void Eprime()
+{
+    if (input[i] == '+')
+    {
+        i++;
+        T();
+        Eprime();
+    }
+}
+void T()
+{
+    F();
+    Tprime();
+}
+void Tprime()
+{
+    if (input[i] == '*')
+    {
+        i++;
+        F();
+        Tprime();
+    }
+}
+void F()
+{
+    if (isalnum(input[i]))
+        i++;
+    else if (input[i] == '(')
+    {
+        i++;
+        E();
+        if (input[i] == ')')
+            i++;
+        else
+            error = 1;
+    }
+    else
+        error = 1;
 }
